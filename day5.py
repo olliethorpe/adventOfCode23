0fdc98c01ce50dbbdff2db31a6a9640f68d2f9d6
Ollie Thorpe <thorpe.ollie@outlook.com>
2023-12-10 16:37:33 +0000

    day 5 part 1

--- day5.py
+++ day5.py
@@ -4,23 +4,22 @@ import re
 
 input = cache_and_read_input(5)
 
-
 seeds_and_maps = re.findall(r":\s+(\d+(?:\s+\d+)+)", input)
-
+# print(seeds_and_maps)
 seeds = [int(x) for x in seeds_and_maps[0].split()]
 
-print(f"All given seeds: {seeds}")
+# print(f"All given seeds: {seeds}")
 
 for seed_index, seed_mapped in enumerate(seeds):
     next_mapping_found = False
-    print(f"checking for seed no: {seed_mapped}")
-    for mappings_index, mappings in enumerate(seeds_and_maps):
-        print(f"Mapping set {mappings_index+1}:")
+    # print(f"checking for seed no: {seed_mapped}")
+    for mappings_index, mappings in enumerate(seeds_and_maps[1:]):
+        # print(f"Mapping set {mappings_index+1}:")
         # print(mappings.split('\n'))
         for mapping_index, mapping in enumerate(mappings.split('\n')):
 
             mapping_set = mapping.split(' ')
-            print(f"Mapping {mapping_index}: {mapping_set}")
+            # print(f"Mapping {mapping_index}: {mapping_set}")
             destination_range_start = int(mapping_set[0])
             source_range_start = int(mapping_set[1])
             range_length = int(mapping_set[2])
@@ -28,23 +27,24 @@ for seed_index, seed_mapped in enumerate(seeds):
 
             # destination_range_end = destination_range_start + range_length
             source_range_end = source_range_start + range_length - 1
-            print(f"source start:{source_range_start}")
-            print(f"source end: {source_range_end}")
-            print(f"range length {range_length}")
-            print(f"destination start{destination_range_start}")
+            # print(f"source start: {source_range_start}")
+            # print(f"source end: {source_range_end}")
+            # print(f"range length {range_length}")
+            # print(f"destination start{destination_range_start}")
 
             if seed_mapped >= source_range_start and seed_mapped <= source_range_end:
-                print(f"New mapping found for seed {seed_mapped}")
+                # print(f"New mapping found for seed {seed_mapped}")
                 diff_from_start = seed_mapped - source_range_start
-                print(f"Difference from source start: {diff_from_start} ")
+                # print(f"Difference from source start: {diff_from_start} ")
                 new_seed_value = destination_range_start + diff_from_start
-                print(f"Therefore new value is {new_seed_value}")
+                # print(f"Therefore new value is {new_seed_value}")
                 next_mapping_found = True
                 seeds[seed_index] = new_seed_value
-                print(f"New seed ")
+                seed_mapped = new_seed_value
+                # print(f"New seed mapped: {seeds}")
                 break
 
-        if next_mapping_found:
-            break
+        # if next_mapping_found:
+        #     break
 
-print(seeds)
+print(min(seeds))
