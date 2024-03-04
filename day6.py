14957d3b4dcf6882b6fb4f5e2d96b3304ae45346
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-03-04 21:18:59 +0000

    day 6

---
+++ day6.py
@@ -0,0 +1,34 @@
+from utils import cache_and_read_input
+
+
+input = cache_and_read_input(6)
+
+data = input.split('\n')[1:-1]
+
+wins = []
+
+time = ''
+record = ''
+
+for race in data:
+    components = race.split()
+    time += str(components[0])
+    record += str(components[1])
+
+speed = 0
+distance = 0
+
+winning_times = 0
+
+while time > 0:
+    speed += 1
+    time -= 1
+    distance = speed * time
+    if distance > record:
+        winning_times += 1
+
+wins.append(winning_times)
+
+print(wins)
+print(36*23*9*37)
+
