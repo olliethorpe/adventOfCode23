349fb5c288ee3d9b1dc19f4944df68cf44b2f623
Ollie Thorpe <thorpe.ollie@outlook.com>
2024-03-04 21:55:55 +0000

    start day 7

--- day7.py
+++ day7.py
@@ -2,3 +2,18 @@ from utils import cache_and_read_input
 
 
 r = cache_and_read_input(7)
+
+
+def find_hand(hand: str):
+    d = {}
+    for card in hand:
+        d[card] = d.get(card, 0) + 1
+    return d
+
+
+hands = r.split('\n')
+
+for hand in hands[:1]:
+    hand, bet = hand.split()
+    hand = find_hand(hand)
+    
